7f93987454d16634aeb10a5dec62ede667b2c265
b1ueb1ues <bblues@live.cn>
2018-12-27 12:53:06 +0800

    tweak a.py

--- mechanics/a.py
+++ mechanics/a.py
@@ -1,3 +1,24 @@
+sword = 0.75
+blade = 0.97
+blade2 = 0.97
+dagger = 0.75
+dagger2 = 0.38
+axe = 1.14
+lance = 0.84
+lance2 = 0.45
+bow = 0.29
+bow2 = 0.37
+wand = 0.98
+staff = 0.69
+
+
+def main():
+    dmg = dagger * 683 *10/6
+    addormult("+x",dmg, 1.25,1.2)
+
+    dmg = dagger2 * 683 *10/6
+    addormult("+x",dmg, 1.25,1.2)
+
 
 def printboost(name, dmg, boost, p=1):
     dmg = dmg / 6.0
@@ -14,7 +35,5 @@ def addormult(name, dmg, boost1, boost2):
     print a,"<"
     print b
 
-
-dmg = 568 * 0.97 *1.03  /6*10
-
-addormult("n",dmg, 1.2,1.2)
+if __name__ == "__main__":
+    main()
