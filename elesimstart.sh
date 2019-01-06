3bf66605052f4549b8ddf3ec6c63cd30ebcd04d6
b1ueb1ues <bblues@live.cn>
2019-01-07 01:03:26 +0800

    add a elesim script

---
+++ elesimstart.sh
@@ -0,0 +1,81 @@
+#!/bin/sh 
+clear
+clear
+
+
+python adv/alain.py -1
+python adv/aoi.py -1
+python adv/euden.py -1
+python adv/ezelith.py -1
+python adv/joe.py -1
+python adv/karl.py -1
+python adv/melsa.py -1
+python adv/mikoto.py -1
+python adv/naveed.py -1
+python adv/renelle.py -1
+python adv/sinoa.py -1
+python adv/vanessa.py -1
+python adv/xania.py -1
+echo '-------------------'
+python adv/amane.py -1
+python adv/elias.py -1
+python adv/h_edward.py -1
+python adv/h_elisanne.py -1
+python adv/irfan.py -1
+python adv/julietta.py -1
+python adv/linus.py -1
+python adv/luca.py -1
+python adv/lucretia.py -1
+python adv/malka.py -1
+python adv/malora.py -1
+python adv/raemond.py -1
+python adv/rawn.py -1
+python adv/ryozen.py -1
+echo '-------------------'
+python adv/althemia.py -1
+python adv/berserker.py -1
+python adv/botan.py -1
+python adv/erik.py -1
+python adv/ieyasu.py -1
+python adv/kleimann.py -1
+python adv/nefaria.py -1
+python adv/orion.py -1
+python adv/rodrigo.py -1
+python adv/sazanka.py -1
+python adv/taro.py -1
+python adv/vice.py -1
+python adv/vida.py -1
+python adv/zace.py -1
+echo '-------------------'
+python adv/celliera.py -1
+python adv/cibella.py -1
+python adv/d_cleo.py -1
+python adv/d_nefaria.py -1
+python adv/d_xander.py -1
+python adv/elisanne.py -1
+python adv/jurota.py -1
+python adv/karina.py -1
+python adv/lily.py -1
+python adv/luther.py -1
+python adv/orsem.py -1
+python adv/pietro.py -1
+python adv/rex.py -1
+python adv/waike.py -1
+python adv/xainfried.py -1
+python adv/xander.py -1
+python adv/zardin.py -1
+echo '-------------------'
+python adv/addis.py -1
+python adv/aeleen.py -1
+python adv/eleonora.py -1
+python adv/francesca.py -1
+python adv/hawk.py -1
+python adv/johanna.py -1
+python adv/louise.py -1
+python adv/maribelle.py -1
+python adv/melody.py -1
+python adv/musashi.py -1
+python adv/nicolas.py -1
+python adv/philia.py -1
+python adv/pia.py -1
+python adv/ranzal.py -1
