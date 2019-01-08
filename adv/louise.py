b21157dfd49bc21f977a3e03a8de826b9cf5043b
b1ueb1ues <bblues@live.cn>
2019-01-08 17:50:01 +0800

    last 1 bow user  c1fs spawm

--- adv/louise.py
+++ adv/louise.py
@@ -36,3 +36,4 @@ if __name__ == '__main__':
         """
     adv_test.test(module(), conf, verbose=0)
 
+
--- adv/philia.py
+++ adv/philia.py
@@ -15,13 +15,21 @@ class Philia(adv.Adv):
         this.dmg_make("o_paralysis",1.8)
 
 
+
 if __name__ == '__main__':
     conf = {}
     conf['acl'] = """
-        `s1, seq=5
-        `s2, seq=5 
-        `s3, seq=5
+        `s1, seq=5 or fsc
+        `s2, seq=5 or fsc
+        `s3, seq=5 or fsc
         """
     adv_test.test(module(), conf, verbose=0)
-
+    module().comment = 'spawn c1+fs'
+    conf['acl'] = """
+        `s1, seq=5 or fsc
+        `s2, seq=5 or fsc
+        `s3, seq=5 or fsc
+        `fs, seq=1
+        """
+    adv_test.test(module(), conf, verbose=0)
 
