2cfae15781b71547ae4502491cd2a257f8a3501c
b1ueb1ues <bblues@live.cn>
2019-01-24 17:38:52 +0800

    combine pia

--- adv/pia.py
+++ adv/pia.py
@@ -6,9 +6,16 @@ def module():
     return Pia
 
 class Pia(Adv):
-    comment = 'do not use fs'
+    comment = 'no fs'
     def init(this):
         energy.Energy(this,{'s2':1},{'s2':1})
+
+    def condition(this):
+        this.conf['acl'] = """
+            `s1, seq=5 and cancel
+            `s3, seq=5 and cancel
+            """
+        return 'no s2'
 
 
 if __name__ == '__main__':
@@ -20,10 +27,4 @@ if __name__ == '__main__':
         """
     adv_test.test(module(), conf, verbose=0)
 
-    module().comment = 'do not use fs and s2'
-    conf['acl'] = """
-        `s1, seq=5 and cancel
-        `s3, seq=5 and cancel
-        """
-    adv_test.test(module(), conf, verbose=0)
 
