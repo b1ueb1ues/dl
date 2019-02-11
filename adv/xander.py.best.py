5c616745752e16c0a82e40e55f0d67029d1ea968
b1ueb1ues <bblues@live.cn>
2019-02-11 22:46:19 +0800

    update best xander

--- adv/xander.py.best.py
+++ adv/xander.py.best.py
@@ -8,22 +8,27 @@ class Xander(adv.Adv):
     conf = {
         "mod_d"   :[('att'  , 'passive' , 0.45)  ,
                     ('crit' , 'chance'  , 0.20)] ,
+
         "mod_a"   : ('fs'   , 'passive' , 0.50) ,
 
+        "mod_a2"   : ('att'   , 'buff' , 0.30) ,
+
         "mod_wp"   : [
-            ('fs'   , 'passive' , 0.30) ,
-            ('sp'   , 'passive' , 0.08) ,
-            ]
+            ('s'   , 'passive' , 0.15) ,
+            ('att' , 'buff'    , 0.20) ,
+            ],
         } 
+
+    def s1_proc(this,e):
+        this.dmg_make('s1_boost',this.conf['s1_dmg']*0.5)
 
 
 if __name__ == '__main__':
     conf = {}
     conf['acl'] = """
-        `s1, sp
-        `s2, sp
-        `s3, sp
-        `fs, seq=3 and cancel
+        `s1
+        `s2
+        `fs, seq=2 and cancel
         """
     adv_test.test(module(), conf, verbose=0)
     exit()
