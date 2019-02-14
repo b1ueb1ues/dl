a5402081f82fe25fa0d4d4d6738a32830391efe4
b1ueb1ues <bblues@live.cn>
2019-02-14 17:37:34 +0800

    add vc to v_orion's condition

--- adv/v_orion.py
+++ adv/v_orion.py
@@ -5,20 +5,29 @@ def module():
     return V_Orion
 
 class V_Orion(adv.Adv):
-
     def init(this):
         this.dmg_make("o_s2_burn",1.8)
         this.dmg_make("o_s2_burn",1.8)
         this.dmg_make("o_s2_burn",1.8)
 
-
     def s2_proc(this, e):
         adv.Buff("double_buff",0.08,15,wide='self').on()
-    
-def s2_proc_withcrown(this, e):
-    adv.Buff("double_buff",0.08,15,wide='self').on()
-    adv.Buff("crown_double_buff",0.08,15,wide='self').on()
 
+    def s2_proc_vc(this, e):
+        adv.Buff("double_buff",0.08,15,wide='self').on()
+        adv.Buff("crown_double_buff",0.08,15,wide='self').on()
+
+    def condition(this):
+        this.conf['wp'] = ('s', 'passive', 0.25) 
+        this.conf['acl'] = """
+            `s1
+            `s2
+            `s3
+            `fs, seq=3 and cancel
+            """
+        this.s2_proc = this.s2_proc_vc
+        return 'Valiant Crown'
+    
 
 if __name__ == '__main__':
     conf = {}
@@ -28,19 +37,4 @@ if __name__ == '__main__':
         `fs, seq=3 and cancel
         """
     adv_test.test(module(), conf, verbose=0)
-
-    module().s2_proc = s2_proc_withcrown
-    module().comment = 'Valiant Crown'
-    conf['acl'] = """
-        `s1, sp
-        `s2, sp
-        `s3, sp
-        `fs, seq=3 and cancel
-        """
-    conf.update({
-        "mod_wp"  : ('s'   , 'passive' , 0.25) ,
-        })
-    adv_test.test(module(), conf, verbose=0)
-
-
 
