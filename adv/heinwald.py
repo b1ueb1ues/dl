34203f685ce2365a2b415d1c89bcc6a65adfe371
b1ueb1ues <bblues@live.cn>
2019-03-12 21:08:30 +0800

    fix heinwald

--- adv/heinwald.py
+++ adv/heinwald.py
@@ -8,7 +8,7 @@ class Heinwald(Adv):
 
     def condition(this):
         this.conf['mod_a'] = ('s','passive',0.35)
-        this.s1_proc = this.c_s1_proc
+        this.s2_proc = this.c_s2_proc
         return 'hp70 & buff all teamate'
 
 
@@ -17,16 +17,20 @@ class Heinwald(Adv):
         this.s2ssbuff = Buff("s2_shapshifts1",1, 10,'ss','ss','self')
 
 
-    def c_s1_proc(this, e):
-        if this.s2ssbuff.get():
-            log('-special','s1_with_s2')
-            Buff('s1teambuff',0.1,10).on()
+    #def c_s1_proc(this, e):
+    #    if this.s2ssbuff.get():
+    #        log('-special','s1_with_s2')
+    #        Buff('s1teambuff',0.1,10).on()
 
-    def s1_proc(this, e):
-        if this.s2ssbuff.get():
-            log('-special','s1_with_s2')
-            Buff('s1teambuff',0.1,10,wide='self').on()
+    #def s1_proc(this, e):
+    #    if this.s2ssbuff.get():
+    #        log('-special','s1_with_s2')
+    #        Buff('s1teambuff',0.1,10,wide='self').on()
 
+    def c_s2_proc(this, e):
+        this.s2ssbuff.on()
+        Buff('s2',0.1,10,wide='team').on()
+        Buff('s2',0.1,10,wide='self').on()
 
     def s2_proc(this, e):
         this.s2ssbuff.on()
@@ -36,7 +40,6 @@ class Heinwald(Adv):
 if __name__ == '__main__':
     conf = {}
     conf['acl'] = """
-        `s2, s1.charged>=s1.sp
         `s1, seq=5
         `s2, seq=5
         `s3, seq=5
