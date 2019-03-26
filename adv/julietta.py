191f828ca1168429ebf96a1ad6e6afd4a0959e87
b1ueb1ues <bblues@live.cn>
2019-03-26 17:15:21 +0800

    update double wp

--- adv/julietta.py
+++ adv/julietta.py
@@ -5,14 +5,15 @@ def module():
     return Julietta
 
 class Julietta(adv.Adv):
-    comment = 'do not use weapon skill and fs'
+    comment = 'do not use fs'
     pass
 
 
 if __name__ == '__main__':
     conf = {}
     conf['acl'] = """
-        `s1 
+        `s1
+        `s3,seq=4
         """
     adv_test.test(module(), conf, verbose=0)
 
