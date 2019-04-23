7f041872da42068b5565fc43162a4787604bd2b9
b1ueb1ues <bblues@live.cn>
2019-04-23 11:51:23 +0800

    add alex

---
+++ adv/alex.py
@@ -0,0 +1,24 @@
+import adv_test
+import adv
+
+def module():
+    return Alex
+
+class Alex(adv.Adv):
+    a1 = ('s',0.35,'hp100')
+    a3 = ('sp',0.05)
+
+
+
+if __name__ == '__main__':
+    conf = {}
+    conf['acl'] = """
+        `s1, seq=5 and cancel or fsc
+        `s2, seq=5 and cancel 
+        `s3, seq=5 and cancel or fsc
+        `fs, seq=5
+        """
+
+    adv_test.test(module(), conf, verbose=0)
+
+
--- create_datacsv.sh
+++ create_datacsv.sh
@@ -92,5 +92,6 @@ python adv/curran.py -2     $1 | tee -a www/$1/data.csv
 python adv/g_ranzal.py -2   $1 | tee -a www/$1/data.csv
 python adv/laranoa.py -2    $1 | tee -a www/$1/data.csv
 python adv/fleur.py -2      $1 | tee -a www/$1/data.csv
+python adv/alex.py -2       $1 | tee -a www/$1/data.csv
 
 
