a2eb413a0eed4921dfd347be96883f12c7778510
b1ueb1ues <bblues@live.cn>
2019-05-09 14:48:27 +0800

    add julietta doublebuff set test (worse)

--- adv/julietta.py.db.py
+++ adv/julietta.py.db.py
@@ -1,18 +1,25 @@
 import adv_test
 import adv
+from slot.a import *
 
 def module():
     return Julietta
 
 class Julietta(adv.Adv):
-    comment = 'no fs & no s2'
-    pass
+    comment = 'do not use fs'
+    conf = {}
+    conf['slots.a'] = KFM()+VC()
+
+    def s2_proc(this, e):
+       adv.Event('defchain')()
+
 
 
 if __name__ == '__main__':
     conf = {}
     conf['acl'] = """
         `s1
+        `s2,seq=5
         `s3,seq=4
         """
     adv_test.test(module(), conf, verbose=0)
