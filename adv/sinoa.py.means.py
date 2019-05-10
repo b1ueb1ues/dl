eb0ab1d08f6163b02b9157d0484ef24f94a342b6
b1ueb1ues <bblues@live.cn>
2019-05-10 18:34:29 +0800

    add means sinoa

---
+++ adv/sinoa.py.means.py
@@ -0,0 +1,29 @@
+import adv_test
+import adv
+from adv import *
+
+def module():
+    return Sinoa
+
+class Sinoa(adv.Adv):
+    a1 = ('a',0.13,'hp100')
+    a3 = ('bt',0.2)
+   # conf = {}
+   # import slot
+   # conf['slots.a'] = slot.a.Bellathorna()+slot.a.RR()
+
+
+    def s1_proc(this, e):
+        adv.Teambuff('s1_att',0.25/4,15,'att').on()
+        adv.Teambuff('s1_crit',0.25/4,10,'crit').on()
+
+
+if __name__ == '__main__':
+    conf = {}
+    conf['acl'] = '''
+        `s2, seq=5 and cancel
+        `s3, seq=5 and cancel
+        '''
+    adv_test.test(module(), conf, verbose=-2, mass=0)
+
+
