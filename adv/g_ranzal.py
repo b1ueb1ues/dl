26454041105d39c0289c7c68ace57bd422ee3d79
b1ueb1ues <bblues@live.cn>
2019-05-10 12:02:24 +0800

    update g_ranzal's action control list

--- adv/g_ranzal.py
+++ adv/g_ranzal.py
@@ -7,9 +7,12 @@ def module():
 
 
 class G_Ranzal(Adv):
+    comment = '2+fs when you do not have s2; s2 when 35%<fs_gauge<65%; 3+fs when you have s2; keep every s1 two bar'
+
     a3 = ('s',0.3)
 
     def init(this):
+        this.ifs1ins2 = 0
         this.gauges = {
                 'x':0,
                 'fs':0,
@@ -66,10 +69,12 @@ class G_Ranzal(Adv):
             this.dmg_make('o_s1_boost',this.conf['s1.dmg']*0.2)
         if boost == 2:
             this.dmg_make('o_s1_boost',this.conf['s1.dmg']*0.8)
+        this.ifs1ins2 = 1
 
     def s2_proc(this, e):
         this.fsacharge = 3
         fs_alt(this)
+        this.ifs1ins2 = 0
 
 
 if __name__ == '__main__':
@@ -81,5 +86,15 @@ if __name__ == '__main__':
         `fs, cancel and seq=2 and this.fsacharge < 0  
         """
         #`fs, cancel and seq=2 and this.fsacharge < 0 and not ( this.gauges['x']>930 and this.gauges['fs']>1000 )
+
+    conf['acl'] = """
+        # fskeep = 0
+        # if not this.ifs1ins2 and this.fsacharge <= 1 : fskeep = 1 
+        `s1, this.gauges['x'] >=1000 and this.gauges['fs'] >= 1000
+        `s2, this.gauges['fs'] >= 300 and this.gauges['fs'] < 800
+        `fs, cancel and seq=3 and this.fsacharge > 0 and not fskeep and this.gauges['fs'] < 1000
+        `fs, cancel and seq=2 and this.fsacharge < 0 and this.gauges['x'] >= this.gauges['fs']
+        `fs, cancel and seq=3 and this.fsacharge < 0 and this.gauges['x'] < this.gauges['fs']
+        """
     adv_test.test(module(), conf, verbose=0, mass=0)
 
