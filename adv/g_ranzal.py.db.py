02cc9d66eeaba2f57f2b524680dc665883ef9f4f
b1ueb1ues <bblues@live.cn>
2019-05-10 12:04:22 +0800

    update g_ranzal_special acl

--- adv/g_ranzal.py.db.py
+++ adv/g_ranzal.py.db.py
@@ -23,13 +23,16 @@ class G_Ranzal(g_ranzal.G_Ranzal):
 if __name__ == '__main__':
     conf = {}
     conf['acl'] = """
+        # fskeep = 0
+        # if not this.ifs1ins2 and this.fsacharge <= 1 : fskeep = 1 
         `s1, this.gauges['x'] >=1000 and this.gauges['fs'] >= 1000
-        `s2, s=1
+        `s2, this.gauges['fs'] >= 300 and this.gauges['fs'] < 800
+        `fs, cancel and seq=3 and this.fsacharge > 0 and not fskeep and this.gauges['fs'] < 1000
+        `fs, cancel and seq=2 and this.fsacharge < 0 and this.gauges['x'] >= this.gauges['fs']
+        `fs, cancel and seq=3 and this.fsacharge < 0 and this.gauges['x'] < this.gauges['fs']
         `s3, fsc
-        `fs, cancel and seq=3 and this.fsacharge > 0
-        `fs, cancel and seq=2 and this.fsacharge < 0  
         """
-        #`fs, cancel and seq=2 and this.fsacharge < 0 and not ( this.gauges['x']>930 and this.gauges['fs']>1000 )
+
     from slot.a import *
     conf['slots.a'] = The_Shining_Overlord() + First_Rate_Hospitality()
     #conf['slots.a'] = VC() + First_Rate_Hospitality()
