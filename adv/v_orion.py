ccadc2577d08207efc613bccbf12070a1028309c
b1ueb1ues <bblues@live.cn>
2019-06-11 12:05:42 +0800

    update v_orion's wp again

--- adv/v_orion.py
+++ adv/v_orion.py
@@ -8,10 +8,6 @@ def module():
 
 class V_Orion(adv.Adv):
 
-    conf = {}
-    conf['slots.a'] = First_Rate_Hospitality()+The_Shining_Overlord()
-
-
     def init(this):
         this.dmg_make("o_s2_burn",0.803*3)
         this.dmg_make("o_s2_burn",0.803*3)
@@ -24,11 +20,20 @@ class V_Orion(adv.Adv):
 
 if __name__ == '__main__':
     conf = {}
+   # conf['acl'] = """
+   #     `s1
+   #     `s2
+   #     `s3
+   #     `fs, seq=3 and cancel
+   #     """
+   # conf['slots.a'] = First_Rate_Hospitality()+The_Shining_Overlord()
+
+    module().comment = 'no s2'
     conf['acl'] = """
         `s1
-        `s2
         `s3
         `fs, seq=3 and cancel
         """
+    conf['slots.a'] = The_Shining_Overlord()+LC()
     adv_test.test(module(), conf, verbose=0)
 
