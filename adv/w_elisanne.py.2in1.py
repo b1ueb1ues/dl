39a59fc8f27ddaa829505318b97a229c742cdda4
b1ueb1ues <bblues@live.cn>
2019-06-20 23:02:13 +0800

    update

--- adv/w_elisanne.py.2in1.py
+++ adv/w_elisanne.py.2in1.py
@@ -7,15 +7,17 @@ def module():
 
 
 class W_Elisanne(Adv):
-    comment = '2863 when FRH+JotS'
+    comment = 'FRH+JotS and 2in1'
 
     conf = {}
-    conf['slot.a'] = First_Rate_Hospitality() + The_Shining_Overlord()
+#    conf['slot.a'] = First_Rate_Hospitality() + The_Shining_Overlord()
+    conf['slot.a'] = JotS() + The_Shining_Overlord()
 
     a1 = ('sp',0.08)
     a3 = ('bc',0.13)
 
     def init(this):
+        this.s2on = 0
         if this.condition('s2 defdown for 10s'):
             this.s2defdown = 1
         else:
@@ -25,6 +27,11 @@ class W_Elisanne(Adv):
     def s2_proc(this, e):
         if this.s2defdown :
             Debuff('s2defdown',0.15,10,1).on()
+        this.s2on = 1
+        Timer(this.s2off).on(10);
+
+    def s2off(this, t):
+        this.s2on = 0
 
 
     def s3_proc(this, e):
@@ -34,10 +41,10 @@ class W_Elisanne(Adv):
 if __name__ == '__main__':
     conf = {}
     conf['acl'] = """
-        `s1,fsc
-        `s2
-        `s3,fsc
-        `fs, seq=3 and cancel
+        `s2, fsc
+        `s1, fsc
+        `s3, fsc
+        `fs, seq=3 and cancel 
     """
 
     adv_test.test(module(), conf, verbose=-2, mass=0)
