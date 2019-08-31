c9413624b9f12e2deed271da5b9b379ba80d69ea
b1ueb1ues <bblues@live.cn>
2019-08-31 16:32:51 +0800

    update new banner

---
+++ adv/victor.py.m.py
@@ -0,0 +1,31 @@
+import adv_test
+from adv import *
+from module.bleed import mBleed as Bleed
+import slot
+
+def module():
+    return Victor
+
+class Victor(Adv):
+    a1 = ('a',0.13,'hp70')
+    conf = {}
+    #conf['slots.a'] = slot.a.RR()+slot.a.Jewels_of_the_Sun()
+
+
+    def prerun(this):
+        random.seed()
+        this.bleed = Bleed("g_bleed",0).reset()
+
+    def s1_proc(this, e):
+        Bleed("s1_bleed", 1.46).on()
+
+
+if __name__ == '__main__':
+    conf = {}
+    conf['acl'] = """
+        `s1
+        `s2,seq=5
+        `s3,seq=5
+        """
+    adv_test.test(module(), conf, verbose=-2, mass=0)
+
