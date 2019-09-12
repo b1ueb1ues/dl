d653880d9bbed9ad6f10b87c9524659d5fb809f0
Mushymato <wildshinobu@gmail.com>
2019-09-12 06:27:13 +0000

    heck

--- api/adv_test_app.py
+++ api/adv_test_app.py
@@ -106,7 +106,7 @@ def get_adv_slotlist():
         dragon_module = getattr(slot.d, adv_ele)
         result['adv']['wt'] = adv_instance.slots.c.wt.lower()
         weap_module = getattr(slot.w, result['adv']['wt'])
-        result['adv']['dra'] = type(adv_instance.slots.d).__qualname__
+        result['adv']['pref_dra'] = type(adv_instance.slots.d).__qualname__
         result['adv']['pref_wep'] = type(adv_instance.slots.w).__qualname__
         result['adv']['pref_wp'] = {
             'wp1': type(adv_instance.slots.a).__qualname__,
