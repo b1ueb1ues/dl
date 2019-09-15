1517199ebb59cf22b6f938453a9a13f919d88bc5
b1ueb1ues <bblues@live.cn>
2019-09-15 14:58:40 +0800

    fix emma wp

--- adv/emma.py
+++ adv/emma.py
@@ -21,7 +21,7 @@ class Emma(Adv):
                         `s3, seq=5
                         """
         else:
-            this.conf.slot.a = HG()+HR()
+            this.conf.slot.a = HG()+BB()
             this.conf['acl'] = """
                         `s1
                         `s3, fsc
