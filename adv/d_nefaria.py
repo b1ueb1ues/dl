8785b68fbb0faa14dc46387369e7ef1837b19a6f
Mushymato <wildshinobu@gmail.com>
2019-10-10 21:15:36 +0000

    small fixes

--- adv/d_nefaria.py
+++ adv/d_nefaria.py
@@ -12,19 +12,11 @@ class D_Nefaria(adv.Adv):
     a1 = ('s',0.25)
     conf = {}
     conf['acl'] = """
-        `s1, seq=5
-        `s3, seq=5
+        `s1, fsc
+        `s3, fsc
+        `fs, seq=4
         """
     conf['slots.d'] = DJ()
-
-    def prerun(this):
-        if this.condition('c4+fs'):
-            this.conf['acl'] = """
-                `s1, fsc
-                `s3, fsc
-                `fs, seq=4
-                """
-
 
 if __name__ == '__main__':
     conf = {}
