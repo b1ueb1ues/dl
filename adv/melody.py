f40f54240510ccf2fb80f246fe75513759956116
mushymato <wildshinobu@gmail.com>
2019-11-30 12:44:32 -0500

    melody

--- adv/melody.py
+++ adv/melody.py
@@ -11,19 +11,10 @@ def module():
 class Melody(adv.Adv):
     comment = 'no s2'
     a1 = ('cc',0.08,'hp100')
-<<<<<<< HEAD
-    conf['acl'] = """
-        `s1
-        `s3
-    """
-    conf['slots.a'] = slot.a.HG()+slot.a.RR()
-    # conf['slots.d'] = slot.d.Hastur()
-=======
 
     conf = {}
     conf['slots.a'] = slot.a.HG()+slot.a.FWHC()
     conf['slots.d'] = slot.d.Zephyr()
->>>>>>> 68c8187458e8944fa9781dcd1b9375d898f912c6
 
 
 if __name__ == '__main__':
