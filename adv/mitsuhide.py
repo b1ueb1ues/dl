21e020acee0de1e460e3d8ca437c688327b3008e
mushymato <wildshinobu@gmail.com>
2020-01-20 18:25:56 -0500

    update mitsu para mods

--- adv/mitsuhide.py
+++ adv/mitsuhide.py
@@ -42,7 +42,7 @@ class Mitsuhide(Adv):
             this.hits += 5
 
     def s1_proc(this, e):
-        this.afflics.paralysis('s1',120, 0.883)
+        this.afflics.paralysis('s1',120, 0.97)
         this.dmg_make('o_s1',0.61*11,'s')
 
     def s2_proc(this, e):
