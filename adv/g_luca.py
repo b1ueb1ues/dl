24a7783d43db95d196b9d131a1eeebb817c5c454
qwewqa <198e559dbd446d973355f415bdfa34@gmail.com>
2020-01-26 06:40:53 +0000

    Make g_luca deterministic

--- adv/g_luca.py
+++ adv/g_luca.py
@@ -19,59 +19,63 @@ class G_Luca(Adv):
         """
 
     def init(this):
-        random.seed()
         this.crit_mod = this.custom_crit_mod
-
-        this.s1_crit_mod = Modifier('mod_s1_cc', 'crit', 'chance', 0)
-        this.s1_crit_mod.get = this.s1_buff_count
-        this.s1_crit_mod.off()
-        
-        this.a1_crit_mod = Modifier('mod_a1_cc', 'crit', 'chance', 0)
-        this.a1_crit_mod.get = this.a1_buff_count
-        this.a1_crit_mod.off()
-
-        this.a1_buffs = [
-            Selfbuff('a1_sylvan',0.03,20,'crit','chance'),
-            Selfbuff('a1_human',0.03,20,'crit','chance'),
-            Selfbuff('a1_rokkan',0.03,20,'crit','chance')
-        ]
-        this.a1_iscding = False
-
-    def prerun(this):
-        this.a1_crit_mod.on()
+        this.in_s1 = False
+        this.a1_buff_types = 3
+        this.a1_states = {(None,) * this.a1_buff_types: 1.0}
 
     def buff_icon_count(this):
         # not entirely accurate to game, but works fine in the scope of s2 + the 3 a1 buffs
         return len(set([b.name for b in this.all_buffs]))
 
-    def s1_buff_count(this):
-        this.mod_value = 0.10 * this.buff_icon_count()
-        return this.mod_value
-
-    def a1_buff_count(this):
-        this.mod_value = min(0.04 * this.buff_icon_count(), 0.28)
-        return this.mod_value
-
-    def a1_cooldown(this, t):
-        this.a1_iscding = False
-        log('cd','a1','end')
-    
     def custom_crit_mod(this):
-        cdmg = this.rand_crit_mod()
-        if cdmg > 1 and not this.a1_iscding:
-            random.choice(this.a1_buffs).on()
-            this.a1_iscding = True
-            Timer(this.a1_cooldown).on(3)
-        return cdmg
+        base_rate = 0.0
+        crit_dmg = 0.7
+        for i in this.all_modifiers:
+            if 'crit' == i.mod_type:
+                if i.mod_order in ['passive', 'rate', 'chance']:
+                    base_rate += i.get()
+                if i.mod_order in ['dmg', 'damage']:
+                    crit_dmg += i.get()
+        new_states = defaultdict(lambda: 0.0)
+        t = now()
+        base_icon_count = this.buff_icon_count()
+        mean_rate = 0.0
+
+        for start_state, state_p in this.a1_states.items():
+            state = tuple([b if b is not None and t - b <= 20.0 * this.bufftime() else None for b in start_state])  # expire old stacks
+            current_rate = base_rate
+            icon_count = base_icon_count
+            a1_buff_count = len([b for b in state if b is not None])  # active a1buff count
+            icon_count += a1_buff_count
+            current_rate += 0.03 * a1_buff_count  # a1buff crit
+            current_rate += min(0.28, 0.04 * icon_count)  # a1 icon crit
+            if this.in_s1:
+                current_rate += 0.1 * icon_count  # s1 icon crit
+            current_rate = min(1.0, current_rate)
+            mean_rate += current_rate * state_p
+
+            if (b := state[0]) is not None and t - b < 3.0:  # proc in last 3 seconds
+                new_states[state] += state_p  # state won't change
+            else:
+                miss_rate = 1.0 - current_rate
+                new_states[state] += miss_rate * state_p
+
+                for i in range(this.a1_buff_types):
+                    # t is the newest buff timing so it's in the front; the rest remain in order
+                    new_states[(t,) + state[0:i] + state[i + 1:]] += current_rate * state_p / this.a1_buff_types
+        this.a1_states = new_states
+
+        return 1.0 + mean_rate * crit_dmg
 
     def s1_proc(this, e):
-        with this.s1_crit_mod:
-            this.dmg_make('s1',3.14*1)
-            this.dmg_make('s1',3.14*1)
-            this.dmg_make('s1',3.14*1)
-            this.dmg_make('s1',3.14*1)
+        this.in_s1 = True
+        this.dmg_make('s1', 3.14)
+        this.dmg_make('s1', 3.14)
+        this.dmg_make('s1', 3.14)
+        this.dmg_make('s1', 3.14)
+        this.in_s1 = False
 
 if __name__ == '__main__':
     conf = {}
-    adv_test.test(module(), conf, verbose=-2, mass=1)
-
+    adv_test.test(module(), conf, verbose=-2, mass=0)
