ff2c2a5e2774cc71444e9fb3bf90bbb29c64bff3
mushymato <wildshinobu@gmail.com>
2020-02-05 18:46:40 -0500

    add buff modes on sinoa 4fun

--- adv/sinoa.py
+++ adv/sinoa.py
@@ -6,7 +6,7 @@ def module():
     return Sinoa
 
 class Sinoa(adv.Adv):
-    comment = '5c+fs'
+    comment = 'buff modes: means/random/att/crit'
 
     a1 = ('a',0.13,'hp100')
     a3 = ('bt',0.2)
@@ -19,21 +19,32 @@ class Sinoa(adv.Adv):
         `fs, seq=5
         '''
 
-    def s1_proc(this, e):
-        adv.Teambuff('s1_att',0.25/4,15,'att').on()
-        adv.Teambuff('s1_crit',0.25/4,10,'crit').on()
+    def prerun(this):
+        this.s1_buff_mode = 'alternate'
+        this.prev_buff = 'crit'
 
-    # def s1_proc(this, e):
-    #     r = random.random()
-    #     if r<0.25  :
-    #         adv.Teambuff('s1_att',0.25,15,'att').on()
-    #     elif r<0.5 :
-    #         adv.Teambuff('s1_crit',0.25,10,'crit').on()
-    #     else:
-    #         log('failed','s1')
+    def s1_proc(this, e):
+        if this.s1_buff_mode == 'means':
+            adv.Teambuff('s1_att',0.25/4,15,'att').on()
+            adv.Teambuff('s1_crit',0.25/4,10,'crit').on()
+        elif this.s1_buff_mode == 'random':
+            r = random.random()
+            if r<0.25  :
+                adv.Teambuff('s1_att',0.25,15,'att').on()
+            elif r<0.5 :
+                adv.Teambuff('s1_crit',0.25,10,'crit').on()
+            elif r<0.75:
+                Event('defchain')()
+            else:
+                log('debug','s1 HP buff')
+        elif this.s1_buff_mode == 'att':
+            adv.Teambuff('s1_att',0.25,15,'att').on()
+        elif this.s1_buff_mode == 'crit':
+            adv.Teambuff('s1_crit',0.25,10,'crit').on()
 
 if __name__ == '__main__':
     conf = {}
+    module().comment = ''
     adv.adv_test.test(module(), conf, verbose=-2)
 
 
