5b5648210ced385f35558cb7f06001fe20423037
Dylan Nagel <dylan_nagel@comcast.com>
2020-02-17 12:16:55 -0500

    dockerize api

--- app/app.py
+++ app/app.py
@@ -15,7 +15,7 @@ import slot.w
 app = Flask(__name__)
 
 # Helpers
-ROOT_DIR = '/home/wildshinobu/dl/'
+ROOT_DIR = '../'
 MEANS_ADV = {
     'addis': 'addis.py.means.py',
     'sazanka': 'sazanka.py.means.py',
