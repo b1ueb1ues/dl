5263265d6be56ba28d9d02e65164e07366b35911
6tennis <6tennis@gmail.com>
2020-03-31 14:38:47 -0400

    update 3* shadow spirals
    Vice OP pls nerf

--- adv/vice.py
+++ adv/vice.py
@@ -1,17 +1,38 @@
 from core.advbase import *
+from slot.a import *
+from slot.d import *
+from module.x_alt import Fs_alt
 
 def module():
     return Vice
 
 class Vice(Adv):
-    a1 = ('bk',0.2)
+    a1 = ('bk',0.35)
     conf = {}
+    conf['slot.a'] = Twinfold_Bonds()+The_Plaguebringer()
+    conf['slot.d'] = Shinobi()
     conf['acl'] = """
         `s3, not self.s3_buff
         `s1
         `s2
         `fs, x=5
         """
+
+    def fs_proc_alt(self, e):
+        self.afflics.poison('fs', 120, 0.582)
+
+    def prerun(self):
+        conf_fs_alt = {'fs.dmg': 0.174, 'fs.hit': 6}
+        self.fs_alt = Fs_alt(self, Conf(conf_fs_alt), self.fs_proc_alt)
+
+    def s1_proc(self, e):
+        with KillerModifier('s1_killer', 'hit', 0.5, ['poison']):
+            self.dmg_make("s1", 15.84)
+
+    def s2_proc(self, e):
+        self.fs_alt.on(1)
+        with KillerModifier('s2_killer', 'hit', 0.5, ['poison']):
+            self.dmg_make("s2", 16.77)
 
 if __name__ == '__main__':
     from core.simulate import test_with_argv
