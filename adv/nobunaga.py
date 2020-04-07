660dbf6f65d8b493671405134112e536c294465f
mushymato <wildshinobu@gmail.com>
2020-04-07 01:18:35 -0400

    nobu s2

--- adv/nobunaga.py
+++ adv/nobunaga.py
@@ -7,6 +7,8 @@ def module():
     return Nobunaga
 
 class Nobunaga(Adv):
+    comment = 'use s2 instead of fs to dispel when possible'
+
     a1 = ('a',0.2,'hit15')
     conf = {}
     conf['slot.a'] = RR()+Primal_Crisis()
@@ -14,7 +16,7 @@ class Nobunaga(Adv):
         `s3, not self.s3_buff
         `s1
         `s2
-        `fs, x=5
+        `fs, x=5 and self.s2.charged<self.s2.sp-self.sp_val(5)
         """
 
     def prerun(self):
@@ -27,6 +29,7 @@ class Nobunaga(Adv):
         if self.ba == 1:
             self.ba = 0
             self.dmg_make('o_s1_boost',11.18)
+            self.dmg_make('o_s2_boost',self.conf.s2.dmg*0.3)
 
     def fs_proc(self, e):
         if self.ba == 1:
