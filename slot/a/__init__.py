201564d1370343fea037f1180505d7a681a19939
mushymato <wildshinobu@gmail.com>
2020-04-14 01:22:13 -0400

    key

--- slot/a/__init__.py
+++ slot/a/__init__.py
@@ -53,8 +53,8 @@ class Amulet(AmuletBase):
         for name, ab_list in sorted_abilities.items():
             if name in self.mmax:
                 max_value = self.mmax[name]
-                for ab in sorted(ab_list, key=lambda x: '' if len(x) < 3 else x[2]):
-                    if len(ab) > 2 and ab[2] not in ('flame', 'water', 'wind', 'light', 'shadow'):
+                for ab in sorted(ab_list, key=lambda x: '' if len(x) < 3 or x[2] in ('flame', 'water', 'wind', 'light', 'shadow') else x[2]):
+                    if len(ab) > 2:
                         new_ab = (ab[0], min(ab[1], max_value), *ab[2:])
                     else:
                         new_ab = (ab[0], min(ab[1], max_value))
