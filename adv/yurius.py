4e839f5d398b94544e74a4453ac2b31810cc34b3
mushymato <wildshinobu@gmail.com>
2020-05-13 08:26:14 -0400

    pause ddrive timer when skill

--- adv/yurius.py
+++ adv/yurius.py
@@ -9,17 +9,20 @@ class Yurius(Adv):
     a3 = ('prep', 100)
     conf = {}
     conf['slots.a'] = Primal_Crisis()+Candy_Couriers()
-    conf['slots.d'] = Gaibhne_and_Creidhne()
+    conf['slots.d'] = Siren()
     conf['slots.frostbite.d'] = Siren()
     conf['acl'] = """
+        if self.afflics.frostbite.get()
         `dragon, not self.dragondrive_buff.get() and self.dragonform.dragon_gauge>=2120
+        else
+        `dragon, self.dragondrive_buff.get()
+        `dragon, not self.dragondrive_buff.get() and s=2
+        end
         `s2, cancel
         `s1, cancel
         `s3, cancel
     """
-    coab = ['Blade','Dagger','Xander']
-    # conf['sim_afflict.efficiency'] = 1
-    # conf['sim_afflict.type'] = 'frostbite'
+    coab = ['Blade','Thaniel','Xander']
 
     def prerun(self):
         # 3000/1200/75
@@ -39,6 +42,7 @@ class Yurius(Adv):
         if self.dragondrive_buff.get():
             with KillerModifier('s1_killer', 'hit', 0.6, ['frostbite']):
                 self.dmg_make('s1', 7.92)
+            self.dragonform.add_drive_gauge_time(self.s1.ac.getstartup()+self.s1.ac.getrecovery(), skill_pause=True)
         else:
             self.dmg_make('s1', 7.56)
             self.dragonform.charge_gauge(530, utp=True)
@@ -48,12 +52,17 @@ class Yurius(Adv):
         if self.dragondrive_buff.get():
             with KillerModifier('s2_killer', 'hit', 0.6, ['frostbite']):
                 self.dmg_make('s2', 10.52)
+            self.dragonform.add_drive_gauge_time(self.s2.ac.getstartup()+self.s2.ac.getrecovery(), skill_pause=True)
         else:
             self.dmg_make('s2', 2.08)
             self.afflics.frostbite('s2',120,0.287,duration=30)
             self.dmg_make('s2', 6.24)
             self.dragonform.charge_gauge(530, utp=True)
             self.inspiration.add(2, team=False)
+
+    def s3_proc(self, e):
+        if self.dragondrive_buff.get():
+            self.dragonform.add_drive_gauge_time(self.s3.ac.getstartup()+self.s3.ac.getrecovery(), skill_pause=True)
 
 if __name__ == '__main__':
     from core.simulate import test_with_argv
